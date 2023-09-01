78d6e36ead4f09ce192b0eb0d533360e2febbfaa
Yudis <yudis10@users.noreply.github.com>
2023-09-01 15:22:25 +0700

    update astro to 3.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,23 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://online.detik.com', //Change to your site URL
+  site: "https://online.detik.com", //Change to your site URL
   vite: {
     build: {
       cssCodeSplit: false,
     },
   },
-  integrations: [sitemap(), tailwind({applyBaseStyles: false})],
-  base: "/design/static/starter-project/preview", //Change to your base path
+  integrations: [sitemap(), tailwind({ applyBaseStyles: false })],
+  base: "/design/static/starter-project/preview/", //Change to your base path
   build: {
     format: "file",
     assets: "_starter-project", //Change to your project name
+    inlineStylesheets: "never",
   },
+  compressHTML: false,
   output: "static",
   outDir: "./preview",
 });
